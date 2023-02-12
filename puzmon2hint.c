4d2cb6288009cf5373c10c38f76eab6f1f9e4700
GotaSuzuki <go.baseball.0408@icloud.com>
2023-02-12 19:19:37 +0900

    2クリア

--- puzmon2hint.c
+++ puzmon2hint.c
@@ -7,11 +7,13 @@
 
 // (a)属性
 // HINT: FIRE, WATERなどを含む列挙型Elementを定義する
+typedef enum Element {FIRE, WATER, WIND, EARTH, LIFE, EMPTY} Element;
 
 /*** グローバル定数の宣言 ***/
 
 // (b)属性別の記号
 // HINT: '$'や'~'を含む配列ELEMENT_SYMBOLSを定義する
+const char ELEMENT_SYMBOLS[EMPTY+1] = {'$','~','@','#','&', ' '};
 
 // (c)属性別のカラーコード（ディスプレイ制御シーケンス用）
 const char ELEMENT_COLORS[EMPTY+1] = {1,6,2,3,5,0};
@@ -20,11 +22,19 @@ const char ELEMENT_COLORS[EMPTY+1] = {1,6,2,3,5,0};
 
 // (f) モンスター
 // HINT: 各種のメンバを持つMonster型配列を定義する
+typedef struct MONSTER{
+  char* name;
+  Element element;
+  int maxhp;
+  int hp;
+  int attack;
+  int defense;
+} Monster;
 
 // (g)ダンジョン
 typedef struct DUNGEON {
-  // HINT: ダンジョンでの登場モンスターを格納した配列の先頭アドレスを格納するメンバの宣言
-  // HINT: 登場モンスターを格納した配列の要素数を格納するメンバ（不変）の宣言
+  Monster* monsters;
+  const int numMonsters;
 } Dungeon;
 
 
@@ -35,6 +45,7 @@ int doBattle(char* playerName, Monster* pEnemy);
 
 // ユーティリティ関数
 // HINT: printMonsterName関数のプロトタイプ宣言
+void printMonsterName(Monster* monster);
 
 /*** 関数宣言 ***/
 
@@ -49,6 +60,14 @@ int main(int argc, char** argv)
   printf("*** Puzzle & Monsters ***\n");
 
   // ダンジョンの準備
+  Monster dungeonMonsters[] = {
+    {"スライム",    WATER, 100, 100, 10,  5},
+    {"ゴブリン",    EARTH, 200, 200, 20, 15},
+    {"オオコウモリ", WIND,  300, 300, 30, 25},
+    {"ウェアウルフ", WIND,  400, 400, 40, 30},
+    {"ドラゴン",    FIRE,  800, 800, 50, 40}
+  };
+  Dungeon dungeon = {dungeonMonsters, 5};
 
   // HINT: 要素数5のモンスター配列dungeonMonstersを宣言すると同時に、
   //       "スライム",    WATER, 100, 100, 10,  5
@@ -57,10 +76,10 @@ int main(int argc, char** argv)
   // HINT: Dungeon型の変数を宣言し、上記配列の先頭アドレスと要素数で初期化。
 
   // いざ、ダンジョンへ
-  int winCount = goDungeon(argv[1], /* HINT: ダンジョンのアドレス */);
+  int winCount = goDungeon(argv[1], &dungeon);
 
   // 冒険終了後
-  if(winCount == /* HINT: ダンジョンのモンスター数 */) {
+  if(winCount == dungeon.numMonsters) {
     printf("***GAME CLEAR!***\n");
   }  else {
     printf("***GAME OVER***\n");
@@ -70,7 +89,7 @@ int main(int argc, char** argv)
 }
 
 // (2)ダンジョン開始から終了までの流れ
-int goDungeon(char* playerName, /* ダンジョンのアドレス */)
+int goDungeon(char* playerName, Dungeon* pDungeon)
 {
   printf("%sはダンジョンに到着した\n", playerName);
 
@@ -79,20 +98,25 @@ int goDungeon(char* playerName, /* ダンジョンのアドレス */)
   // HINT: ダンジョンにいる全モンスターについて繰り返し {
   // HINT:     i番目のモンスターとdoBattle関数で戦い、結果の勝利数をwinCountに加える
   // HINT: }
+  for(int i=0; i<pDungeon->numMonsters; i++){
+    winCount += doBattle(playerName, &(pDungeon->monsters[i]));
+  }
 
   printf("%sはダンジョンを制覇した！\n", playerName);
   return winCount;
 }
 
 // (3)バトル開始から終了までの流れ
-int doBattle(char* playerName, /* 戦う敵モンスターのアドレス */)
+int doBattle(char* playerName, Monster* pEnemy)
 {
   // HINT: モンスター名を表示する関数を呼び出す;
+  printMonsterName(pEnemy);
   printf("が現れた！\n");
 
   // ダミーのため速攻倒す
 
   // HINT: モンスター名を表示する関数を呼び出す;
+  printMonsterName(pEnemy);
   printf("を倒した！\n");
   return 1;
 }
@@ -100,15 +124,19 @@ int doBattle(char* playerName, /* 戦う敵モンスターのアドレス */)
 /*** ユーティリティ関数宣言 ***/
 
 // (A)モンスター名のカラー表示
-void printMonsterName(/* モンスターのアドレス */)
+void printMonsterName(Monster* pMonster)
 {
   // HINT: char型変数symbolを宣言し、引数で指定されたモンスターの
   //       属性記号($や#など）を取得する。
   //       （ELEMENT_SYMBOLS配列から取り出す）
+  char symbol = ELEMENT_SYMBOLS[pMonster->element];
 
   // HINT: char型変数colorを宣言し、引数で指定されたモンスターの
   //       カラーコード(1や6など）を取得する。
   //       （ELEMENT_COLORS配列から取り出す）
+  printf("\x1b[3%dm", ELEMENT_COLORS[pMonster->element]);
+  printf("%c%s%c", symbol, pMonster->name, symbol);
+  printf("\x1b[0m");
 
   // HINT: ディスプレイ制御シーケンスを使い、文字色を変更
   // HINT: 「属性記号 モンスター名 属性記号」と表示
